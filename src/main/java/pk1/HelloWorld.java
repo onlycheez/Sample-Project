f168464825aa064f52762be3b2d0f9087cfaff70
Jan Šidlo <sidlojan42@gmail.com>
2021-01-03 16:39:25 +0100

    Fix HellowWorld indendation

--- src/main/java/pk1/HelloWorld.java
+++ src/main/java/pk1/HelloWorld.java
@@ -1,19 +1,18 @@
 package main.java.pk1;
 
 public class HelloWorld {
-private String name;
+  private String name;
 
-     public static void main(String [] args) {
-     System.out.println(new HelloWorld("Test").sayHello());
-         System.out.println("Bye");
- //      more  changes to test
- //df;glkh;gdshj;
-     }
+  public static void main(String [] args) {
+    System.out.println(new HelloWorld("Test").sayHello());
+    System.out.println("Bye");
+  }
+
   public HelloWorld(String name) {
-      this.name = name;
+    this.name = name;
   }
 
   public String sayHello() {
-      return "Hello, " + name;
+    return "Hello, " + name;
   }
 }
